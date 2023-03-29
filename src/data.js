89eda01bcbd80be5d7093fbc49f1be124e0976bb
sder12 <sara.cetto@gmail.com>
2023-03-29 19:39:22 +0200

    Update data and style

--- src/data.js
+++ src/data.js
@@ -1,37 +1,37 @@
 const data = [
     {
         id: 1,
-        nome: "Francesco Robiola",
-        stato: "No pain no Gain XOXO PALESTRA MUSCOLI POTENZA",
+        nome: "Francesco Robiola GM",
+        stato: "Update on the status of the new product launch",
         img:
             "https://res.cloudinary.com/thomasdea/image/upload/v1612341193/react-api-course/tizio-palestra_naoqwo.jpg",
     },
     {
         id: 2,
-        nome: "Sabrina Cortelli",
-        stato: "La musica è per l'anima quello che la ginnastica è per il corpo.",
+        nome: "Sabrina Cortelli CEO",
+        stato: "Review of the company's performance in the past quarter",
         img:
             "https://res.cloudinary.com/thomasdea/image/upload/v1612341197/react-api-course/tizia-cuffie_xf2bae.jpg",
     },
     {
         id: 3,
-        nome: "Mike Russel",
-        stato: "Non permettere a nessuno di dirti che non sei capace",
+        nome: "Mike Russel COO",
+        stato: "Discussion of the new product launch strategy",
         img:
             "https://res.cloudinary.com/thomasdea/image/upload/v1612341184/react-api-course/tizio-giacca_jvzktv.jpg",
     },
     {
         id: 4,
-        nome: "Rebecca Orlando",
+        nome: "Rebecca Orlando DESIGN",
         stato:
-            "La fotografia è passione e amore, è l’emozione passeggera, che arriva e subito vola via.",
+            "Update on the progress of the social media campaign",
         img:
             "https://res.cloudinary.com/thomasdea/image/upload/v1612341204/react-api-course/tipa-occhiali_svc9jp.jpg",
     },
     {
         id: 5,
-        nome: "Crhistian Minerva",
-        stato: "Non mangerei un hamburger nemmeno se mi pagassero 40,000 dollari",
+        nome: "Crhistian Minerva SVP",
+        stato: "Brainstorming session for the upcoming email marketing campaign",
         img:
             "https://res.cloudinary.com/thomasdea/image/upload/v1612341200/react-api-course/tizio_awlq7r.jpg",
     },
