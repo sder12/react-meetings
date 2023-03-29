063ffcb5a31ee40c758ddc64036473d72446fbe3
sder12 <sara.cetto@gmail.com>
2023-03-29 18:18:59 +0200

    Add given data and style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import List from "./List";
+import data from "./data";
+
 function App() {
   return (
     <div className="App">
       <h2>
         Title
       </h2>
+
+      <List />
     </div>
   );
 }
